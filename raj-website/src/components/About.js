5a18e6b773cc52d98492bb45416c5c5d63213c87
zionsenpai <rajvishwakarma303@gmail.com>
2023-03-05 09:53:51 +0530

    Added components and styles

---
+++ raj-website/src/components/About.js
@@ -0,0 +1,88 @@
+import React from "react";
+import "../styles/About.css";
+import FadeInSection from "./FadeInSection";
+
+class About extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      expanded: true,
+      activeKey: "1"
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  handleSelect(eventKey) {
+    this.setState({
+      activeKey: eventKey
+    });
+  }
+  render() {
+    const one = (
+      <p>
+        I am currently a <b>Software Development Engineer</b> at
+        <a href="https://www.aboutamazon.com/"> Amazon</a>, working in the AWS
+        sector under team Route53. At the same time, I am undertaking a
+        part-time <b> Master's of Science</b> in <b>Software Engineering </b>at{" "}
+        <a href="https://www.ox.ac.uk/about">University of Oxford</a>.
+      </p>
+    );
+    const two = (
+      <p>
+        Outside of work, I'm interested in following the developments of
+        science. I also play a lot of video games. And make TikToks.
+      </p>
+    );
+    const three = (
+      <p>
+        <b>Want to chat?</b> Shoot me a message at{" "}
+        <a href="mailto:gazi.jarin@mail.utoronto.ca">
+          gazi.jarin@mail.utoronto.ca
+        </a>{" "}
+        and let's talk.
+      </p>
+    );
+    const desc_items = [one, two];
+
+    const tech_stack = [
+      "Javascript ES6+",
+      "Python",
+      "React.js",
+      "Java",
+      "Node.js",
+      "HTML & CSS"
+    ];
+
+    const tech_items = tech_stack.map(stack => <li>{stack}</li>);
+
+    return (
+      <div id="about">
+        <FadeInSection>
+          <div className="section-header ">
+            <span className="section-title">/ about me</span>
+          </div>
+          <div className="about-content">
+            <div className="about-description">
+              {[one]}
+              {"Here are some technologies I have been working with:"}
+              <ul className="tech-stack">
+                {tech_stack.map(function (tech_item, i) {
+                  return (
+                    <FadeInSection delay={`${i + 1}00ms`}>
+                      <li>{tech_item}</li>
+                    </FadeInSection>
+                  );
+                })}
+              </ul>
+              {[two]}
+            </div>
+            <div className="about-image">
+              <img src={"/assets/me2.jpg"} />
+            </div>
+          </div>
+        </FadeInSection>
+      </div>
+    );
+  }
+}
+
+export default About;
---
+++ raj-website/src/components/Intro.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+import "../styles/Intro.css";
+import Typist from "react-typist";
+import "react-typist/dist/Typist.css";
+import GaziModel from "./GaziModel";
+import EmailRoundedIcon from "@material-ui/icons/EmailRounded";
+import FadeInSection from "./FadeInSection";
+import FractalTree from "./FractalTree";
+
+class Intro extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      expanded: true,
+      activeKey: "1",
+      visible: true
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  handleSelect(eventKey) {
+    this.setState({
+      activeKey: eventKey
+    });
+  }
+  render() {
+    return (
+      <div id="intro">
+        <FractalTree></FractalTree>
+        <Typist avgTypingDelay={120}>
+          <span className="intro-title">
+            {"hi, "}
+            <span className="intro-name">{"gazi"}</span>
+            {" here."}
+          </span>
+        </Typist>
+        <FadeInSection>
+          <div className="intro-subtitle">I create stuff sometimes.</div>
+          <div className="intro-desc">
+            I'm a software engineer and artist based in Toronto. I have great
+            interest in full-stack development, artificial intelligence,
+            human-computer interactions, and everything in between.
+          </div>
+          <a
+            href="mailto:gazi.jarin@mail.utoronto.ca"
+            className="intro-contact"
+          >
+            <EmailRoundedIcon></EmailRoundedIcon>
+            {"  " + "Say hi!"}
+          </a>
+        </FadeInSection>
+      </div>
+    );
+  }
+}
+
+export default Intro;
